0391bb97ce0ee4aca123e44c9407159bcedd20e1
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2023-12-03 15:15:43 -0500

    cleaned up

--- day3/p1.cpp
+++ day3/p1.cpp
@@ -8,21 +8,13 @@ using namespace std;
 
 vector<vector<char> > readFile() {
     ifstream file("./p1.txt");
-
     string line;
     vector<vector<char> > blueprint;
-    cout << endl;
     while (getline(file, line)) {
         vector<char> current_line;
-        for (char character : line) {
-            cout << character << " ";
-            current_line.push_back(character);
-        }
+        for (char character : line) current_line.push_back(character);
         blueprint.push_back(current_line);
-        cout << endl;
     }
-    cout << endl << endl;
-
     file.close();
     return blueprint;
 }
@@ -32,29 +24,27 @@ bool isNum(char c) {
     return regex_match(string(1, c), pattern);
 }
 
-bool isChar(char c) {
-    regex pattern("\\.");
-    return !regex_match(string(1, c), pattern) && !isNum(c);
+bool isGear(char c) {
+    char asterisk = '*';
+    return c == asterisk;
 }
 
 int makeIntAndRemove(vector<vector<char> >& bp, int row, int left, int right) {
     string numString;
-    int num;
     char period = '.';
 
     for (left; left < right; left++) {
         numString += bp[row][left];
         bp[row][left] = period;
-        // cout << bp[row][left];
     }
-    // cout << numString << ", ";
 
-    return stoi(numString);
-    
+    return stoi(numString);  
 }
 
 int searchForNums(vector<vector<char> >& bp, int symbolRow, int symbolCol) {
     int sum = 0;
+    vector<int> partList;
+
     for (int rowMod = -1; rowMod <= 1; rowMod++) {
         int row = symbolRow + rowMod;
         int left, right;
@@ -62,18 +52,19 @@ int searchForNums(vector<vector<char> >& bp, int symbolRow, int symbolCol) {
             if (isNum(bp[row][col])){
                 left = col;
                 right = col;
-                while (isNum(bp[row][right]) && right < bp[row].size()) {
-                    right++;
-                }
+                while (isNum(bp[row][right]) && right < bp[row].size()) right++;
                 col = right - 1;
                 if (
                 ((left >= symbolCol - 1 && left <= symbolCol + 1) && left <= right) ||
                 ((right >= symbolCol && right <= symbolCol + 2) && right >= left)
-                ) {
-                    sum += makeIntAndRemove(bp, row, left, right);
-                }
+                ) partList.push_back(makeIntAndRemove(bp, row, left, right));
             }
         }
+    }
+
+    if (partList.size() > 1) {
+        sum = partList[0];
+        for (int i = 1; i < partList.size(); i++) sum *= partList[i];
     }
     return sum;
 }
@@ -84,14 +75,16 @@ int main() {
     //read file and create matrix
     vector<vector<char> > bp = readFile();
 
+    //search for gears in matrix
     int sum = 0;
     for (int row = 0; row < bp.size(); row++) {
         for (int col = 0; col < bp[row].size(); col++) {
-            if (isChar(bp[row][col])) {
+            if (isGear(bp[row][col])) {
                 sum += searchForNums(bp, row, col);
             }  
         }
     }
 
+    cout << "Sum = " << sum << endl;
     return 0;
 }
